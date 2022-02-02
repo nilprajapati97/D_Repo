2f603ae716cd7b2c4af4272d3424d6855ee4cd1a
nilprajapati97 <nil.prajapati97@yahoo.com>
2022-02-02 07:00:48 +0530

    First_Day

---
+++ 0001_Day_Introduction/1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+//global variable
+void my_function(int); // Function_Declaration
+int main()
+{
+
+        //local variable
+	printf("Hello World\n");
+	// Logic  y= a+b;
+	// function call my_function(y);
+}
+
+void my_fucntion(int k)  // Function defination
+{
+
+printf("In function print value k ");
+
+
+}
+
