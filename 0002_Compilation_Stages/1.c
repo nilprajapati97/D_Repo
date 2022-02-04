581f9e2eef0edb85ef57eb4ca9f3e0b091ee8558
nilprajapati97 <nil.prajapati97@yahoo.com>
2022-02-04 07:05:34 +0530

    knojakjofanfjkcan
    Signed-off-by: nilprajapati97 <nil.prajapati97@yahoo.com>

---
+++ 0002_Compilation_Stages/1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define VAL 3   // macro 
+int  main()
+{
+
+ 	int no=10,a;
+        
+        a=no+VAL;
+	 /*
+	  *Hwloo How are you
+	  *knmskdnkosndkos
+	  * mkmkpmpk
+	  *
+	  * */
+  //       int pre=system(ls);
+	printf("Hello World...\n");
+	printf("====================output============.\n");
+	printf("Output =%d\n",a);
+         // ANIl is here
+
+	return 0;
+}
+
